d4b5a09173937dca1ed1bd9dd1b29b090cf4fabb
TycTheSaint <tycewebster@gmail.com>
2023-10-12 12:49:30 -0500

    Recent Changes

--- List.h
+++ List.h
@@ -1,16 +1,24 @@
+/*
+	Title:  List.h
+	Author:  Tyce Webster & Isaac Essex
+    Course: CSC 1310-001
+	Date:  10/12/2023
+    Due: 10/12/2023
+	Purpose: This header file serves as the framework for our doubly-linked list.
+*/
 #ifndef LIST_H
 #define LIST_H
 
 #include <iostream>
 using namespace std;
 
-template <typename T>
+template <class T> 
 class List
 {
     private:
         struct ListNode
         {
-            T value;
+            T *value;
             ListNode* next;
             ListNode* prev;
         };
@@ -29,112 +37,21 @@ class List
         }
         ~List(); //Deconstructor
         void print();
-        void append(T);
+        void append(T*);
         void mergesort();
 };
 
 template <typename T>
 void List<T>::print(ListNode* nodePtr) //Parameter moves through the list
 {
-    nodePtr = head; //Sets nodePtr to the head of the list
+    ListNode *temp= nodePtr; //Sets iterator to the head of the list
 
     //While nodePtr points to a node, traverse the list
-    while(nodePtr)
+    while(temp)
     {
-        cout << nodePtr->value << endl; //Displays the value of a node
-
-        nodePtr = nodePtr->next; //Sets the nodePtr to the next node of the list
+        cout << temp->value->getPopulation() << endl; //Displays the value of a node
+        temp = temp->next; //Sets the nodePtr to the next node of the list
     }
-}
-
-template <typename T>
-typename List<T>::ListNode* List<T>::mergesort(ListNode* first, ListNode* last)
-{
-    ListNode* mid;
-    ListNode* left;
-    ListNode* right;
-
-    //Base case for when list partition is zero or one node, function returns first parameter
-    if(first == NULL || first == last)
-    {
-        return first;
-    }
-
-    //Splits the list into two partitions and sort each partition recursively
-    mid = split(first, last);
-    left = mergesort(first, mid);
-    right = mergesort(mid->next, last);
-
-    //Merges the sorted partitions and returns the merged list
-    return merge(left, right);
-}
-
-template <typename T>
-typename List<T>::ListNode* List<T>::merge(ListNode* first, ListNode* last)
-{
-    ListNode* merged = NULL;
-
-    //Base case if either node is NULL, then return the other nodes
-    if(first == NULL)
-    {
-        return last;
-    }
-    else if(last == NULL)
-    {
-        return first;
-    }
-
-    //Compares the value of the nodes
-    if(first->value > last->value)
-    {
-        merged = first;
-        merged->next = merge(first->next, last);
-        merged->next->prev = merged;
-    }
-    else
-    {
-        merged = last;
-        merged->next = merge(first, last->next);
-        merged->next->prev = merged;
-    }
-
-    //Sets the previous pointer of merged node to NULL
-    merged->prev = NULL;
-
-    return merged;
-}
-
-template <typename T>
-typename List<T>::ListNode* List<T>::split(ListNode* first, ListNode* last)
-{
-    ListNode* slow;
-    ListNode* fast;
-    ListNode* mid;
-
-    if(first == NULL || last == NULL || first == last)
-    {
-        return NULL; //Returns NULL if the partition is empty or has only one node
-    }
-
-    slow = first;
-    fast = first->next;
-
-    //Traverses the list with two pointers, one moving one step at a time, the other moving two steps at a time
-    while(fast != last && fast->next != last)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    mid = slow->next;
-    slow->next = NULL; //Sets the next pointer of node before mid to NULL to split the list
-
-    if(mid != NULL)
-    {
-        mid->prev = NULL; //Sets the previous pointer of mid to NULL
-    }
-
-    return mid; //Returns the middle node
 }
 
 template <typename T>
@@ -162,21 +79,22 @@ void List<T>::print()
 }
 
 template <typename T>
-void List<T>::append(T newValue)
+void List<T>::append(T* newValue)
 {
-    ListNode* newNode; //Points to a new node
+    ListNode *newNode; //Points to a new node
 
     //Allocates newNode and stores newValue there
     newNode = new ListNode;
-    newNode->value = newValue;
+    newNode->value= newValue;
     newNode->next = NULL;
     newNode->prev = NULL;
 
     //If there are no nodes in the list, then make newNode the first node
     if(!head)
     {
-        head = newNode;
+        head=newNode;
         tail = newNode;
+        cout<<"\n\nno head\n\n";
     }
     else //Inserts newNode at the end of the list
     {
@@ -200,6 +118,93 @@ void List<T>::mergesort()
     {
         tail = tail->next;
     }
+}
+
+template <typename T>
+typename List<T>::ListNode* List<T>::mergesort(ListNode* first, ListNode* last)
+{
+    ListNode* mid;
+    ListNode* left;
+    ListNode* right;
+
+    //Base case for when list partition is zero or one node, function returns first parameter
+    if(first == NULL || first == last)
+    {
+        cout<<"\n\nreturn mergesort\n\n";
+        return first;
+    }
+
+    //Splits the list into two partitions and sort each partition recursively
+    mid = split(first, last);
+    left = mergesort(first, mid);
+    right = mergesort(mid->next, last);
+
+    //Merges the sorted partitions and returns the merged list
+    cout<<"\n\nmerge partitions\n\n";
+    return merge(left, right);
+}
+
+template <typename T>
+typename List<T>::ListNode* List<T>::split(ListNode* first, ListNode* last)
+{
+    cout<<"\nenter split\n";
+    ListNode* mid;
+
+    if(first == NULL || last == NULL || first == last)
+    {
+        cout<<"\nempty or one node split\n";
+        return first; //Returns NULL if the partition is empty or has only one node
+    }
+
+    //Traverses the list with two pointers, one moving one step at a time, the other moving two steps at a time
+
+    while(first!=last && first->next!=last)
+    {
+        first=first->next;
+        last=last->prev;
+    }
+
+    mid = last->prev;
+    mid->next->prev=NULL;
+    mid->next=NULL;
+
+    return mid; //Returns the middle node
+}
+
+template <typename T>
+typename List<T>::ListNode* List<T>::merge(ListNode* first, ListNode* last)
+{
+    ListNode* merged;
+
+    //Base case if either node is NULL, then return the other nodes
+    if(first == NULL)
+    {
+        return last;
+    }
+    else if(last == NULL)
+    {
+        return first;
+    }
+
+    merged = NULL;
+    //Compares the value of the nodes
+    if(first->value <= last->value)
+    {
+        merged = first;
+        merged->next = merge(first->next, last);
+        merged->next->prev = merged;
+    }
+    else
+    {
+        merged = last;
+        merged->next = merge(first, last->next);
+        merged->next->prev = merged;
+    }
+
+    //Sets the previous pointer of merged node to NULL
+    merged->prev = NULL;
+
+    return merged;
 }
 
 #endif
